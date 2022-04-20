47d824035ac98e5a112b96b0a37e5a97004e674d
adalinmik <msuda100@gmail.com>
2022-04-20 16:57:46 +0200

    Add JsonSave

---
+++ src/aft2022t/JsonSave.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aft2022t;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class JsonSave {
+    
+    private static String fileName = "save.json";
+    
+    private static boolean saveFile(){
+        ArrayList<String[]> dobasok = Dobokocka.getDobasok();
+        
+        return true;
+    }
+    
+}
