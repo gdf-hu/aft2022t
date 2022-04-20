71dcfeb13569663edc92a12ef9916324347e466b
guttidani <61143051+guttidani@users.noreply.github.com>
2022-04-20 12:15:42 +0200

    Update Dobokocka.java
    dobas->arraylist<int>

--- src/aft2022t/Dobokocka.java
+++ src/aft2022t/Dobokocka.java
@@ -12,10 +12,9 @@ import java.util.*;
  * @author Dani
  */
 public class Dobokocka {
-    
-    private static int KockaOldal;
-    private static int KockaDarab;
-    private static ArrayList<Integer> Dobasok = new ArrayList<Integer>();
+
+    private static int KockaOldal = 6;
+    private static int KockaDarab = 1;
 
     public static int getKockaOldal() {
         return KockaOldal;
@@ -32,22 +31,27 @@ public class Dobokocka {
     public static void setKockaDarab(int KockaDarab) {
         Dobokocka.KockaDarab = KockaDarab;
     }
-    
-    public Dobokocka (int oldal){
-        this.KockaOldal=oldal;
-        this.KockaDarab=1;
+
+    public Dobokocka(int oldal) {
+        this.KockaOldal = oldal;
+        this.KockaDarab = 1;
     }
-    
-    public Dobokocka (int oldal,int darab){
-        this.KockaOldal=oldal;
-        this.KockaDarab=darab;
+
+    public Dobokocka(int oldal, int darab) {
+        this.KockaOldal = oldal;
+        this.KockaDarab = darab;
     }
-    
-    public static int dobas(int oldal){
+
+    public static ArrayList<Integer> dobas() throws InterruptedException {
+        ArrayList<Integer> lista = new ArrayList<Integer>();
         int min = 1;
         int max = Dobokocka.KockaOldal;
         Random rnd = new Random();
-        Dobokocka.Dobasok.add(rnd.nextInt(max-min+1));
-        return rnd.nextInt(max-min+1);
+        for (int i = 0; i < Dobokocka.KockaDarab; i++) {
+            //Thread.sleep(rnd.nextLong(300));
+            lista.add(rnd.nextInt(max - min + 1));
+        }
+
+        return lista;
     }
 }
