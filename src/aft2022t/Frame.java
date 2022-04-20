0bbb8c3f6a910f0e6b12db4b2b784c562bda167d
guttidani <61143051+guttidani@users.noreply.github.com>
2022-04-20 13:27:39 +0200

    Revert "Merge branch 'main' of https://github.com/gdf-hu/aft2022t"
    This reverts commit bbe24c91c27158174644c782fea26154df194c70, reversing
    changes made to 657c58a231fa219ab29ab7d91f30f2569dcdeb0e.

--- src/aft2022t/Frame.java
+++ src/aft2022t/Frame.java
@@ -5,9 +5,6 @@
 package aft2022t;
 
 import java.awt.Label;
-import java.util.ArrayList;
-import javax.swing.JDialog;
-import javax.swing.JLabel;
 
 /**
  *
@@ -22,9 +19,6 @@ public class Frame extends javax.swing.JFrame {
         initComponents();
         Dobokocka.setKockaOldal(6);
         Dobokocka.setKockaDarab(1);
-
-        jSpinnerOldal.setValue(Dobokocka.getKockaOldal());
-        jSpinnerDobás.setValue(Dobokocka.getKockaDarab());
     }
 
     /**
@@ -36,30 +30,28 @@ public class Frame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jButtonDobás = new javax.swing.JButton();
-        jSpinnerOldal = new javax.swing.JSpinner();
+        jButton1 = new javax.swing.JButton();
+        jSpinner1 = new javax.swing.JSpinner();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        jSpinnerDobás = new javax.swing.JSpinner();
+        jSpinner2 = new javax.swing.JSpinner();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jListDobások = new javax.swing.JList<>();
+        jList1 = new javax.swing.JList<>();
         jLabel3 = new javax.swing.JLabel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Dobókocka");
 
-        jButtonDobás.setText("Dobás");
-        jButtonDobás.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setText("Dobás");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButtonDobásActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
 
-        jSpinnerOldal.addChangeListener(new javax.swing.event.ChangeListener() {
+        jSpinner1.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                jSpinnerOldalStateChanged(evt);
+                jSpinner1StateChanged(evt);
             }
         });
 
@@ -67,41 +59,22 @@ public class Frame extends javax.swing.JFrame {
 
         jLabel2.setText("Dobás");
 
-        jSpinnerDobás.addChangeListener(new javax.swing.event.ChangeListener() {
+        jSpinner2.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                jSpinnerDobásStateChanged(evt);
+                jSpinner2StateChanged(evt);
             }
         });
 
         jScrollPane1.setToolTipText("");
 
-        jListDobások.setModel(new javax.swing.AbstractListModel<String>() {
+        jList1.setModel(new javax.swing.AbstractListModel<String>() {
             String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
             public int getSize() { return strings.length; }
             public String getElementAt(int i) { return strings[i]; }
         });
-        jScrollPane1.setViewportView(jListDobások);
+        jScrollPane1.setViewportView(jList1);
 
         jLabel3.setText("Dobások:");
-
-        jMenu1.setText("About");
-        jMenu1.addMenuListener(new javax.swing.event.MenuListener() {
-            public void menuCanceled(javax.swing.event.MenuEvent evt) {
-            }
-            public void menuDeselected(javax.swing.event.MenuEvent evt) {
-            }
-            public void menuSelected(javax.swing.event.MenuEvent evt) {
-                jMenu1MenuSelected(evt);
-            }
-        });
-        jMenu1.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jMenu1ActionPerformed(evt);
-            }
-        });
-        jMenuBar1.add(jMenu1);
-
-        setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -111,11 +84,11 @@ public class Frame extends javax.swing.JFrame {
                 .addGap(39, 39, 39)
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSpinnerOldal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSpinnerDobás, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(57, 57, 57))
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -125,7 +98,7 @@ public class Frame extends javax.swing.JFrame {
                             .addGroup(layout.createSequentialGroup()
                                 .addGap(10, 10, 10)
                                 .addComponent(jLabel3))
-                            .addComponent(jButtonDobás)))
+                            .addComponent(jButton1)))
                     .addGroup(layout.createSequentialGroup()
                         .addGap(157, 157, 157)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 83, javax.swing.GroupLayout.PREFERRED_SIZE)))
@@ -136,58 +109,39 @@ public class Frame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGap(14, 14, 14)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jSpinnerOldal, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jSpinner1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1)
                     .addComponent(jLabel2)
-                    .addComponent(jSpinnerDobás, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jSpinner2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
-                .addComponent(jButtonDobás)
+                .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jLabel3)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(32, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButtonDobásActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonDobásActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         System.out.println("Dobás!");
         Label text = new Label();
         text.setText("1");
-        try {
-            ArrayList<Integer> lista = Dobokocka.dobas();
-            System.out.println(lista);
-        } catch (Exception e) {
-        }
-
+        System.out.println(Dobokocka.dobas(2));
         //jList1.add(new ListItem("Item "));
-    }//GEN-LAST:event_jButtonDobásActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
-    private void jSpinnerOldalStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinnerOldalStateChanged
-        System.out.println("Oldal: " + jSpinnerOldal.getValue().toString());
-        Dobokocka.setKockaOldal((int) jSpinnerOldal.getValue());
-    }//GEN-LAST:event_jSpinnerOldalStateChanged
+    private void jSpinner1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner1StateChanged
+        System.out.println("Oldal: " + jSpinner1.getValue().toString());
+        Dobokocka.setKockaOldal((int)jSpinner1.getValue());
+    }//GEN-LAST:event_jSpinner1StateChanged
 
-    private void jSpinnerDobásStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinnerDobásStateChanged
-        System.out.println("Dobás: " + jSpinnerDobás.getValue().toString());
-        Dobokocka.setKockaDarab((int) jSpinnerDobás.getValue());
-    }//GEN-LAST:event_jSpinnerDobásStateChanged
-
-    private void jMenu1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenu1ActionPerformed
-
-    }//GEN-LAST:event_jMenu1ActionPerformed
-
-    private void jMenu1MenuSelected(javax.swing.event.MenuEvent evt) {//GEN-FIRST:event_jMenu1MenuSelected
-        System.out.println("Open about");
-       JDialog  dialog = new JDialog (this, "About");
-       
-        dialog.add(new JLabel("Dobókocka \n Verzió: 1.0 \n Készítették:"));
-        
-        dialog.setSize(400, 200);
-        dialog.setVisible(true);
-    }//GEN-LAST:event_jMenu1MenuSelected
+    private void jSpinner2StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jSpinner2StateChanged
+        System.out.println("Dobás: " + jSpinner2.getValue().toString());
+        Dobokocka.setKockaDarab((int)jSpinner2.getValue());
+    }//GEN-LAST:event_jSpinner2StateChanged
 
     /**
      * @param args the command line arguments
@@ -225,15 +179,13 @@ public class Frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButtonDobás;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JList<String> jListDobások;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JList<String> jList1;
     private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JSpinner jSpinnerDobás;
-    private javax.swing.JSpinner jSpinnerOldal;
+    private javax.swing.JSpinner jSpinner1;
+    private javax.swing.JSpinner jSpinner2;
     // End of variables declaration//GEN-END:variables
 }
