7408d0e83a9f50af19fe4d27222146c433841509
adalinmik <msuda100@gmail.com>
2022-04-20 11:31:39 +0200

    Add project

---
+++ src/aft2022t/Aft2022t.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aft2022t;
+
+/**
+ *
+ * @author PC
+ */
+public class Aft2022t {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
